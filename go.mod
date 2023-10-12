bcf957dbb751eeca63106b44ca82744d3f58c743
KiwiKid <KiwiKid@users.noreply.github.com>
2023-10-13 09:58:49 +1300

    start building out pages

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/KiwiKid/go-dev-site
+
+go 1.21.1
+
+require github.com/a-h/templ v0.2.364 // indirect
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/a-h/templ"
+)
+
+func main() {
+
+
+
+    component := hello("John")
+    
+    http.Handle("/", templ.Handler(component))
+
+	aboutPage := aboutPage()
+
+	http.Handle("/about", templ.Handler(aboutPage))
+
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+
+
+    fmt.Println("Listening on :3000")
+    if err := http.ListenAndServe(":3000", nil); err != nil {
+        log.Printf("error listening: %v", err)
+    }
+}
